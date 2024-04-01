f891d475a1481999b046b746265f17ca92f83e38
D0KVA <isip_v.s.sokolov@mpt.ru>
2024-04-02 01:45:44 +0300

    first comment

---
+++ LabPW3/LabPW3/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LabPW3.DataSet1TableAdapters;
+
+namespace LabPW3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GamesTableAdapter g = new GamesTableAdapter(); 
+        public MainWindow()
+        {
+            InitializeComponent();
+            dg.ItemsSource = g.GetFullInfo();
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dg.Columns[0].Visibility = Visibility.Collapsed;
+            dg.Columns[2].Visibility = Visibility.Collapsed;
+            dg.Columns[3].Visibility = Visibility.Collapsed;
+
+            dg.Columns[1].Header = "Название игры";
+            dg.Columns[4].Header = "Жанр";
+            dg.Columns[5].Header = "Описание жанра";
+        }
+    }
+}
---
+++ PW3Entity/PW3Entity/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PW3Entity
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PractikaPW1Entities context = new PractikaPW1Entities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dg.ItemsSource = context.Games.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
